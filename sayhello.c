3ff2b29b04324e0391a00e6764ede6db05e44b8f
Jwill175 <Jwill175@lion.lmu.edu>
2025-08-27 15:56:25 -0700

    Added code

---
+++ sayhello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main( int argc, char * argv[] ) {
+
+   char   input[25];
+   printf( "Hi! What's your name: " );
+   scanf ("%s", input);
+   printf( "\n Hello, %s\n", input );
+   exit( 0 );
+
+}
